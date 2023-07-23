04cd90a0464f4320e1f4fefd8d3df93693e7f34f
Ömer Necmi KAYHAN <kayhan@disqet.com>
2023-07-23 18:58:23 +0300

    Frontend

--- frontend/admin/app/data-table.tsx
+++ frontend/admin/app/data-table.tsx
@@ -102,7 +102,7 @@ export function DataTable<TData, TValue>({
             <div className="flex items-center justify-end space-x-2 py-4">
                 {
                     Array.from({length: totalPages}, (_, i) => i + 1).map((page) => (
-                        <Button onClick={changePage(page)} variant={currentPage === page ? "" : "secondary"}>{page}</Button>
+                        <Button onClick={changePage(page)} variant={currentPage === page ? "default" : "secondary"}>{page}</Button>
                     ))
                 }
             </div>
